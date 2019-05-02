19ab3446ac0e90ffe9e7e3866b54e17a85aa70ce
juany-ramirez <juany.ramrz@unitec.edu>
2019-05-01 21:44:02 -0600

    Agregando bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home/Home';
-
+import Pets from './components/Pets/Pets';
 class App extends Component {
   render(){
     return (
       <div className="App">
         <h1>React App</h1>
         <Home></Home>
+        <Pets></Pets>
       </div>
     );
   }
---
+++ client/src/components/Pets/Pets.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Pets extends Component {
+  
+    constructor(){
+        super();
+        this.state = {
+            pets: []
+        }
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className="App">
+            
+            </div>
+        );
+    }
+}
+
+export default Pets;
